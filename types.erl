a40c862ae9b2dc5a6b1416fcd2fb8f16859c2e02
fabiokleis <fabiohkrc@gmail.com>
2023-12-22 00:19:02 -0300

    types erlang exercism series

---
+++ types.erl
@@ -0,0 +1,45 @@
+-module(types).
+-export([convert_l_to_i/1]).
+-export([convert_l_to_bits/1]).
+-export([convert_bits_to_list/1]).
+-export([convert_t_to_list/1]).
+-export([series/2]).
+-export([series_tuple/4]).
+
+%% is_builtin(module, fun, arity).
+%% is_builtin(erlang, is_tuple, 1).
+%% is_builtin(erlang, is_bitstring, 1).
+%% is_builtin(erlang, is_list, 1).
+%% is_builtin(erlang, is_integer, 1).
+%% is_builtin(erlang, is_float, 1).
+
+
+convert_l_to_i(V) ->
+    integer_to_list(V). %% modulo padrao erlang
+
+convert_l_to_bits(V) ->
+    erlang:list_to_bitstring(V). 
+
+convert_bits_to_list(A) ->
+    bitstring_to_list(A).
+
+convert_t_to_list(L) ->
+    tuple_to_list(L).
+
+series_tuple(Dst, Str, N, Acc) ->
+     L = length(Str) + 1,
+     case L of
+ 	L when L == N -> Dst;
+ 	_ -> series_tuple(erlang:insert_element(Acc, Dst, lists:sublist(Str, N)), tl(Str), N, Acc+1)
+     end.
+
+%% exercism
+serie(Dst, Str, N) ->
+    L = length(Str) + 1,
+    case L of
+	L when L == N -> Dst;
+	_ -> serie(lists:append(Dst, [lists:sublist(Str, N)]), tl(Str), N)
+    end.
+
+series(N, Str) when length(Str) > 0 andalso length(Str) >= N ->
+    serie([], Str, N).
