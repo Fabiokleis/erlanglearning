64466b462dd5cd1aa20511828866e7c13caf962d
fabiokleis <fabiohkrc@gmail.com>
2023-12-27 20:24:25 -0300

    function matching version of collatz conjecture

--- collatz_conjecture.erl
+++ collatz_conjecture.erl
@@ -2,14 +2,20 @@
 
 -export([steps/1]).
 
-count_steps(N, Acc) ->
-    case N of
-        1 -> Acc;
-        A when A rem 2 == 0 -> 
-            count_steps(N div 2, Acc+1);
-        A when A rem 2 /= 0 ->
-            count_steps(N*3 + 1, Acc+1)
-    end.
+count_steps(1, Acc) -> Acc;
+count_steps(N, Acc) when N rem 2 == 0 -> 
+    count_steps(N div 2, Acc+1);
+count_steps(N, Acc) when N rem 2 /= 0 ->
+    count_steps(N * 3 + 1, Acc+1).
+
+%%count_steps(N, Acc) ->
+%%    case N of
+%%        1 -> Acc;
+%%        A when A rem 2 == 0 -> 
+%%            count_steps(N div 2, Acc+1);
+%%        A when A rem 2 /= 0 ->
+%%            count_steps(N*3 + 1, Acc+1)
+%%    end.
 
 steps(N) when N =< 0 -> error(badarg); 
 steps(N) -> 
