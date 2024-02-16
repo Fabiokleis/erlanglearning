13771f9b13610407889704026f7ee808e467e743
fabiokleis <fabiohkrc@gmail.com>
2024-02-15 22:18:18 -0300

    exercism forth suporta: - definições recursivas; - consegue ter definições em ordem, definições posteriores não alteram;   definições já feitas; falta: - sobrecarga das funcoes basicas; - validacao do durante a avaliacao, por exemplo operadores binarios;

--- forth.erl
+++ forth.erl
@@ -38,57 +38,51 @@ stack_ins(W) ->
 lookup_table(Name, Table) ->
     lists:filter(fun({N, _}) -> N == Name end, Table).
 
-lexer([], Stack, Table) -> {lists:reverse(Stack), Table};
+lexer([], Stack, Table) -> {Stack, Table};
 lexer([H|T], Stack, Table) ->
-    %%io:format("instruction: ~p~nstack: ~p~ntable: ~p~n", [H, Stack, Table]),
+    %%io:format("head: ~p~nstack: ~p~ntable: ~p~n~n", [H, Stack, Table]),
     case stack_ins(H) of
 	{lit, Int} -> lexer(T, [{lit, Int}|Stack], Table);
 	{unknown_instruction, Ins} -> 
 	    case lookup_table(Ins, Table) of
 		[] -> error(undefined_instruction);
-		[{Name, _}] -> lexer(T, [{def, Name}|Stack], Table)
+		[{_, Values}] -> lexer(T, Values ++ Stack, Table)
 	    end;
 	ins -> 
 	    Nins = lists:takewhile(fun(C) -> C =/= ";" end, T), %% simply read until ;
 	    case lookup_table(hd(Nins), Table) of
 		[] -> 
-		    {Values, _} = lexer(tl(Nins), [], Table),
+		    {Values, _} = lexer(tl(Nins), Stack, Table),
 		    lexer(lists:nthtail(length(Nins) + 1, T), Stack, [{hd(Nins), Values}|Table]);
 		[{Name, Values}] -> 
-		    %% pls support recursion
-		    if hd(tl(Nins)) == hd(Nins) -> todo;
-		       true ->
-			    {TValues, _} = lexer(tl(Nins), [], Table),
-			    lexer(lists:nthtail(length(Nins) + 1, T), Stack, [{hd(Nins), TValues}|lists:delete({Name, Values}, Table)])
-		    end
+		    {StackValues, _} = lexer(tl(Nins), [], Table),
+		    lexer(
+		      lists:nthtail(length(Nins) + 1, T), Stack,
+		      [{hd(Nins), StackValues}|lists:delete({Name, Values}, Table)]
+		     )
 	    end;
 
 	_ -> lexer(T, [{ins, H}|Stack], Table)
     end.
 
 %% evaluator
-eval([], _, Evaluated) -> Evaluated;
-eval([H|T], Table, Evaluated) ->
-    %%io:format("stack: ~p~nevaluated: ~p~n", [H, Evaluated]),
+eval([], Evaluated) -> lists:reverse(Evaluated);
+eval([H|T], Evaluated) ->
+    io:format("stack: ~p~nevaluated: ~p~n", [H, Evaluated]),
     case H of
-	{lit, Num} -> eval(T, Table, [Num|Evaluated]);
+	{lit, Num} -> eval(T, [Num|Evaluated]);
 	{ins, Name} -> 
 	    case stack_ins(Name) of
-		add -> eval(T, Table, add(Evaluated));
-		sub -> eval(T, Table, sub(Evaluated));
-		mul -> eval(T, Table, mul(Evaluated));
-		divi -> eval(T, Table, divi(Evaluated));
-		dup -> eval(T, Table, dup(Evaluated));
-		drop -> eval(T, Table, drop(Evaluated));
-		swap -> eval(T, Table, swap(Evaluated));
-		over -> eval(T, Table, over(Evaluated))
+		add -> eval(T, add(Evaluated));
+		sub -> eval(T, sub(Evaluated));
+		mul -> eval(T, mul(Evaluated));
+		divi -> eval(T, divi(Evaluated));
+		dup -> eval(T, dup(Evaluated));
+		drop -> eval(T, drop(Evaluated));
+		swap -> eval(T, swap(Evaluated));
+		over -> eval(T, over(Evaluated))
 	    end;
-	{def, Name} -> 
-	    case lookup_table(Name, Table) of
-		[] -> error(undefined_instruction);
-		[{_, Values}] -> eval(Values ++ T, Table, Evaluated)
-	    end;
-	_ -> error(wtf)
+	_ -> error(wtf) %% impossivel chegar aqui meu consagrado
     end.
 
 evaluate(Instructions) -> 
@@ -96,5 +90,5 @@ evaluate(Instructions) ->
       fun(Ins, {Stack, Table}) -> 
 	      lexer(string:lexemes(Ins, " "), Stack, Table)
       end, {[], []}, Instructions),
-    io:format("stack: ~p~ntable: ~p~n", [S, T]),
-    eval(S, T, []).
+    io:format("stack: ~p~ntable: ~p~n", [lists:reverse(S), T]),
+    eval(lists:reverse(S), []).
