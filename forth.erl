d532a5232dda41d1c6b07c85eaf6f7e04769cd1c
fabiokleis <fabiohkrc@gmail.com>
2024-02-17 18:09:46 -0300

    forth: feito verificacao do numero de argumentos e suportado redefinição case insensitive

--- forth.erl
+++ forth.erl
@@ -35,12 +35,11 @@ stack_ins(W) ->
 	Int -> {lit, Int}
     end.
 
-lookup_table(Name, Table) ->
-    lists:filter(fun({N, _}) -> N == Name end, Table).
+lookup_table(Name, Table) -> lists:filter(fun({N, _}) -> N == Name end, Table).
 
 lexer([], Stack, Table) -> {Stack, Table};
 lexer([H|T], Stack, Table) ->
-    io:format("head: ~p~nstack: ~p~ntable: ~p~n~n", [H, Stack, Table]),
+    %%io:format("head: ~p~nstack: ~p~ntable: ~p~n~n", [H, Stack, Table]),
     case stack_ins(H) of
 	{lit, Int} -> lexer(T, [Int|Stack], Table);
 	{unknown_instruction, Ins} -> 
@@ -49,24 +48,23 @@ lexer([H|T], Stack, Table) ->
 		[{_, Values}] -> lexer(T, Values ++ Stack, Table)
 	    end;
 	ins -> 
-
 	    Nins = lists:takewhile(fun(C) -> C =/= ";" end, T), %% simply read until ;
-	    case stack_ins(hd(Nins)) of
-		{lit, _} -> error(invalid_redefine);
+	    InstName = string:lowercase(hd(Nins)),
+	    case stack_ins(InstName) of
+		{lit, _} -> error(invalid_redefinition); %% only support redefine to string
 		_ -> 
-		    case lookup_table(hd(Nins), Table) of
+		    case lookup_table(InstName, Table) of
 			[] -> 
 			    {Values, _} = lexer(tl(Nins), Stack, Table),
-			    lexer(lists:nthtail(length(Nins) + 1, T), Stack, [{hd(Nins), Values}|Table]);
+			    lexer(lists:nthtail(length(Nins) + 1, T), Stack, [{InstName, Values}|Table]);
 			[{Name, Values}] -> 
 			    {StackValues, _} = lexer(tl(Nins), Stack, Table),
 			    lexer(
 			      lists:nthtail(length(Nins) + 1, T), Stack,
-			      [{hd(Nins), StackValues}|lists:delete({Name, Values}, Table)]
+			      [{InstName, StackValues}|lists:delete({Name, Values}, Table)]
 			     )
 		    end
 	    end;
-
 	_ -> 
 	    case lookup_table(H, Table) of
 		[] -> lexer(T, [H|Stack], Table);
@@ -79,14 +77,14 @@ eval([], Evaluated) -> lists:reverse(Evaluated);
 eval([H|T], Evaluated) when is_integer(H) -> eval(T, [H|Evaluated]);
 eval([H|T], Evaluated) ->
     case stack_ins(H) of
-	add -> eval(T, add(Evaluated));
-	sub -> eval(T, sub(Evaluated));
-	mul -> eval(T, mul(Evaluated));
-	divi -> eval(T, divi(Evaluated));
-	dup -> eval(T, dup(Evaluated));
-	drop -> eval(T, drop(Evaluated));
-	swap -> eval(T, swap(Evaluated));
-	over -> eval(T, over(Evaluated))
+	add when length(Evaluated) > 1 -> eval(T, add(Evaluated));
+	sub when length(Evaluated) > 1 -> eval(T, sub(Evaluated));
+	mul when length(Evaluated) > 1 -> eval(T, mul(Evaluated));
+	divi when length(Evaluated) > 1 -> eval(T, divi(Evaluated));
+	dup when length(Evaluated) > 0 -> eval(T, dup(Evaluated));
+	drop when length(Evaluated) > 0 -> eval(T, drop(Evaluated));
+	swap when length(Evaluated) > 1 -> eval(T, swap(Evaluated));
+	over when length(Evaluated) > 1 -> eval(T, over(Evaluated))
     end.
 
 evaluate(Instructions) -> 
@@ -94,5 +92,8 @@ evaluate(Instructions) ->
       fun(Ins, {Stack, Table}) -> 
 	      lexer(string:lexemes(Ins, " "), Stack, Table)
       end, {[], []}, Instructions),
-    io:format("stack: ~p~ntable: ~p~n", [lists:reverse(S), T]),
-    eval(lists:reverse(S), []).
+    io:format("stack: ~p~ntable: ~p~n", [S, T]),
+    case catch eval(lists:reverse(S), []) of
+	{'EXIT', _} -> error(not_enough_arguments);
+	Evaluated -> Evaluated
+    end.
