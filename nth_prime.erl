decb3823c51db56ba5b415b2720151809a6fe865
fabiokleis <fabiohkrc@gmail.com>
2024-04-17 23:32:20 -0300

    exercicio de discreta, produto dos n primeiros primos + 1 é primo? 2 * 3 * 5 * 7 * 11 * 13 + 1 = 30031, divisivel por 59

--- nth_prime.erl
+++ nth_prime.erl
@@ -6,15 +6,18 @@ next_prime(2) -> 2;
 next_prime(N) ->
     case [X || X <- lists:seq(2, trunc(math:sqrt(N))), N rem X == 0] of
         [] -> N;
-        _ -> []
+        Q -> {racional, {N, hd(Q)}}
     end.
 
-prime(0, _, [H|_]) -> H;
+prime(0, _, Primes) -> lists:foldl(fun(X, S) -> X * S end, 1, Primes) + 1;
 prime(N, Acc, Primes) ->
     case next_prime(Acc+1) of
-        [] -> prime(N, Acc+1, Primes);
+        {racional, _} -> prime(N, Acc+1, Primes);
         P -> prime(N-1, Acc+1, [P|Primes])
     end.
 
 prime(N) when N > 0 ->
-    prime(N, 1, []).
+    case next_prime(prime(N, 1, [])) of
+	{racional, Q} -> {racional, Q};
+	P -> {primo, P}
+    end.
